44f48b7a52f5d9e50af5c15cae11530a41a51a21
eugene masuda <eugene0531masuda@yahoo.co.jp>
2020-08-22 11:50:27 +0900

    indent rubocop

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,26 +1,24 @@
 class ReviewsController < ApplicationController
-
-    def create
-        order = Order.find(review_params[:order_id])
-
-        if order && current_user.id == order.buyer.id
-            if Review.exists?(order_id: review_params[:order_id], buyer_id: current_user.id)
-                flash[:alert] = "You already made the review for this order"
-            else
-                review = Review.new(review_params)
-                review.gig = order.gig
-                review.buyer = current_user
-                review.seller = order.seller
-            end
-        else
-            flash[:alert] = "Invalid order"
-        end
-        redirect_to request.referrer
+  def create
+    order = Order.find(review_params[:order_id])
+    if order && current_user.id == order.buyer.id
+      if Review.exists?(order_id: review_params[:order_id], buyer_id: current_user.id)
+        flash[:alert] = "You already made the review for this order"
+      else
+        review = Review.new(review_params)
+        review.gig = order.gig
+        review.buyer = current_user
+        review.seller = order.seller
+      end
+    else
+      flash[:alert] = "Invalid order"
     end
+    redirect_to request.referrer
+  end
 
-    private
+  private
 
-    def review_params
-        params.require(:review).permit(:stars, :review, :order_id)
-    end
+  def review_params
+    params.require(:review).permit(:stars, :review, :order_id)
+  end
 end
