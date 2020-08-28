d372149ffdfe10ae49306d40305ba9af78bf52cc
eugene masuda <eugene0531masuda@yahoo.co.jp>
2020-08-28 17:06:41 +0900

    private messsage

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,10 @@ Rails.application.routes.draw do
   post '/settings/payment', to: 'users#update_payment', as: "update_payment"
   post '/settings/payout', to: 'users#update_payout', as: "update_payout"
   post '/users/withdraw', to: 'users#withdraw', as: 'withdraw'
+  post 'messages', to: 'messages#create'
+  post '/comments', to: 'comments#create'
+  post '/subscribe', to: 'subscriptions#subscribe'
+  post '/webhook', to: 'subscriptions#webhook'
 
   put '/orders/:id/complete', to: 'orders#complete', as: 'complete_order'
   put '/offers/:id/accept', to: 'offers#accept', as: 'accept_offer'
