379781580108b1a6ed86347e4358bb3bbb8abc5f
eugene masuda <eugene0531masuda@yahoo.co.jp>
2020-08-30 17:26:12 +0900

    rubocop

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,24 +5,22 @@ class MessagesController < ApplicationController
     end
 
     conversation = Conversation.where("(sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?)",
-                                      current_user.id, message_params[:receiver_id], 
-                                      message_params[:receiver_id], current_user.id
-                                    ).first
+                                      current_user.id, message_params[:receiver_id],
+                                      message_params[:receiver_id], current_user.id).first
     if !conversation.present?
       conversation = Conversation.create(sender_id: current_user.id, receiver_id: message_params[:receiver_id])
     end
 
-    @message = Message.new(user_id: current_user.id, 
+    @message = Message.new(user_id: current_user.id,
                            conversation_id: conversation.id,
-                           content: message_params[:content]
-    )
+                           content: message_params[:content])
 
     if @message.save
       conversation.update!(updated_at: @message.created_at)
       receiver = conversation.sender.id == current_user.id ? conversation.receiver : conversation.sender
 
-      MessageChannel.broadcast_to conversation, 
-                                  sender_id: current_user.id, 
+      MessageChannel.broadcast_to conversation,
+                                  sender_id: current_user.id,
                                   sender: render_message(@message, current_user),
                                   receiver: render_message(@message, receiver)
 
