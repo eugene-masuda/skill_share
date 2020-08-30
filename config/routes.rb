72d2b5224f1207b4ad813f2e3c0fab8d40934928
eugene masuda <eugene0531masuda@yahoo.co.jp>
2020-08-30 17:45:38 +0900

    comment function

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'users#dashboard'
   get '/selling_orders', to: 'orders#selling_orders'
   get '/buying_orders', to: 'orders#buying_orders'
+  get '/orders/:id', to: 'orders#show', as: "order_detail"
   get '/all_requests', to: 'requests#list'
   get '/search', to: 'pages#search'
   get '/my_offers', to: 'requests#my_offers'
