426f542ffe1d3b8428ce01bf1d1bbf8ec16787fd
eugene masuda <eugene0531masuda@yahoo.co.jp>
2020-09-09 21:51:48 +0900

    admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,24 +12,6 @@
 #    )
 #end
 
- Create dummy user
-5.times do
-   user = User.create(
-      full_name: Faker::Name.name,
-     email: Faker::Internet.free_email,
-        about: Faker::Quote.matz,
-        password: '123456',
-        from: Faker::Address.country,
-        language: Faker::Nation.language,
-        created_at: Date.today
-    )
-
-    user.avatar.attach(                
-        io: image = open("https://i.pravatar.cc/300"),
-        filename: "avatar#{user.id}.jpg", 
-        content_type: 'image/jpg'
-    )
-end
 
 
   
