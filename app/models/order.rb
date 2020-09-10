6d874d16abd62155c61cbb2de1ce913ee544dbab
eugene masuda <eugene0531masuda@yahoo.co.jp>
2020-09-10 15:41:59 +0900

    review improve

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,6 @@ class Order < ApplicationRecord
   belongs_to :request, required: false
   belongs_to :buyer, class_name: "User"
   belongs_to :seller, class_name: "User"
+  has_many :reviews
   enum status: [:inprogress, :completed]
 end
