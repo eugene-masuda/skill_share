e44ac36da1091a8a64bbef9c1eda49f50fafee48
eugene masuda <eugene0531masuda@yahoo.co.jp>
2020-09-10 16:13:02 +0900

    review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,24 +1,32 @@
 class ReviewsController < ApplicationController
+
   def create
-    order = Order.find(review_params[:order_id])
-    if order && current_user.id == order.buyer.id
-      if Review.exists?(order_id: review_params[:order_id], buyer_id: current_user.id)
-        flash[:alert] = "You already made the review for this order"
+      order = Order.find(review_params[:order_id])
+
+      if order && current_user.id == order.buyer.id
+          if Review.exists?(order_id: review_params[:order_id], buyer_id: current_user.id)
+              flash[:alert] = "You already made the review for this order"
+          else
+              review = Review.new(review_params)
+              review.gig = order.gig
+              review.buyer = current_user
+              review.seller = order.seller
+
+              if review.save
+                  flash[:notice] = "Saved..."
+              else
+                  flash[:alert] = "Cannot create review"
+              end
+          end
       else
-        review = Review.new(review_params)
-        review.gig = order.gig
-        review.buyer = current_user
-        review.seller = order.seller
+          flash[:alert] = "Invalid order"
       end
-    else
-      flash[:alert] = "Invalid order"
-    end
-    redirect_to request.referrer
+      redirect_to request.referrer
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:stars, :review, :order_id)
+      params.require(:review).permit(:stars, :review, :order_id)
   end
 end
