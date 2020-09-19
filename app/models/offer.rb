a9877b59b620498c356948462286dfdde03ac91f
eugene masuda <eugene0531masuda@yahoo.co.jp>
2020-09-19 15:32:34 +0900

    offer page style change

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,6 +2,6 @@ class Offer < ApplicationRecord
   belongs_to :request
   belongs_to :user
 
-  enum status: [:pending, :accepted, :rejected]
+  enum status: [:保留中, :認証, :拒否]
   validates :amount, :days, numericality: { only_integer: true, message: "must be a number" }
 end
